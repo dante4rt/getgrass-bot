5a00a369ee2a48dec265801720211223bea1d02f
Dante4rt <rxmxdhxni@gmail.com>
2024-03-17 13:16:40 +0700

    fix: adjust config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-const userId = '60bc86de-01a3-4006-9b26-7b0386c6398d';
+const userId = 'your_getgrass_user_id';
 const httpProxyList = [
-  'http://PRIM_8WE2F4JAWM-cc-HK-pool-p2p:YV77DPD0G9C9ZH@eventsasia.primedproxies.com:8888',
-  'http://PRIM_8WE2F4JAWM-cc-SG-pool-p2p:YV77DPD0G9C9ZH@eventsasia.primedproxies.com:8888',
+  'http://username:password@hostname:port',
   // Add more proxy URLs if needed
 ];
 
