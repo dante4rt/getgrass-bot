629514dfeb9cf9e8d958178d90cdf50617a2a4d2
Dante4rt <rxmxdhxni@gmail.com>
2024-11-11 18:58:26 +0700

    chore: update desktop version and adjust wss list

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,10 @@
 class Config {
   constructor() {
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.wssHost = 'proxy.wynd.network:4444';
+    this.wssList = ['proxy.wynd.network:4444', 'proxy.wynd.network:4650'];
     this.retryInterval = 20000;
+    this.wssHost =
+      this.wssList[Math.floor(Math.random() * this.wssList.length)];
   }
 }
 
